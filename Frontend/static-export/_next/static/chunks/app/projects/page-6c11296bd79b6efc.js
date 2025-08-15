cdf7039d0cff237a98e5b0910d25c15c0d3d591b
mwasobaddy <kelvinramsiel@gmail.com>
2025-08-15 19:04:12 +0300

    Cleanup: Remove Frontend and root files, update Backend and API, refactor authentication and gallery upload flow

--- Frontend/static-export/_next/static/chunks/app/projects/page-6c11296bd79b6efc.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[893],{3797:(e,s,n)=>{Promise.resolve().then(n.bind(n,5752)),Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,3063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[455,660,539,874,608,752,441,684,358],()=>s(3797)),_N_E=e.O()}]);
